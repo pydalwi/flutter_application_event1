e220958423a208833d90ed27e35e7464d2058454
pydalwi <yodialwiono13@gmail.com>
2022-06-01 00:49:55 +0700

    commit3

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_application_event1/services/theme_services.dart';
+import 'package:flutter_application_event1/ui/home_page.dart';
+import 'package:flutter_application_event1/ui/theme.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() => runApp(new MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Event App',
-      home: Scaffold(),
-      debugShowCheckedModeBanner: false,
-    );
+    return GetMaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: Themes.light,
+        darkTheme: Themes.dark,
+        themeMode: ThemeService().theme,
+        home: HomePage());
   }
 }
---
+++ lib/ui/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static final light =
+      ThemeData(primaryColor: Colors.red, brightness: Brightness.light);
+
+  static final dark =
+      ThemeData(primaryColor: Colors.yellow, brightness: Brightness.dark);
+}
