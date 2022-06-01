e8e892792f5dee4b10698aa739796794eff347b8
pydalwi <yodialwiono13@gmail.com>
2022-06-01 12:42:40 +0700

    thememode

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,13 @@ import 'package:flutter_application_event1/ui/theme.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -5,10 +5,11 @@ import 'package:get/get.dart';
 class ThemeService {
   final _box = GetStorage();
   final _key = 'isDarkMode';
-
+  _saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
   bool _loadThemeFromBox() => _box.read(_key) ?? false;
   ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
   void switchTheme() {
     Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
+    _saveThemeToBox(!_loadThemeFromBox());
   }
 }
