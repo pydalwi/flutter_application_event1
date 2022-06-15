1ed75201b4745fcdbe084de7688f7d7beb02aa2e
pydalwi <yodialwiono13@gmail.com>
2022-06-16 01:33:27 +0700

    Add Login&SignUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_event1/login.dart';
+import 'package:flutter_application_event1/register.dart';
 import 'package:flutter_application_event1/services/theme_services.dart';
 import 'package:flutter_application_event1/ui/home_page.dart';
 import 'package:flutter_application_event1/ui/theme.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:flutter_application_event1/login.dart';
+import 'package:flutter_application_event1/register.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +25,11 @@ class MyApp extends StatelessWidget {
         theme: Themes.light,
         darkTheme: Themes.dark,
         themeMode: ThemeService().theme,
-        home: HomePage());
+        home: HomePage(),
+        initialRoute: 'login',
+        routes: {
+          'login': (context) => const MyLogin(),
+          'register': (context) => const MyRegister(),
+        });
   }
 }
