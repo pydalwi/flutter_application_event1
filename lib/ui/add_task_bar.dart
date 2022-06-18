fa1ff5394166fbcfd385ff3efc3afff197977bc0
MhRivaldo <aldorival279@gmail.com>
2022-06-18 15:49:05 +0700

    menambah kolom Repeat

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -22,6 +22,15 @@ class _AddTaskPageState extends State<AddTaskPage> {
     15,
     20,
   ];
+
+  String _selectedRepeat = "None";
+  List<String> repeatList = [
+    "None",
+    "Daily",
+    "Weekly",
+    "Monthly",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -113,6 +122,34 @@ class _AddTaskPageState extends State<AddTaskPage> {
                       return DropdownMenuItem<String>(
                           value: value.toString(),
                           child: Text(value.toString()));
+                    }).toList(),
+                  ),
+                ),
+                MyInputField(
+                  title: "Repeat",
+                  hint: "$_selectedRepeat",
+                  widget: DropdownButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: subTitleStyle,
+                    underline: Container(
+                      height: 0,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRepeat = newValue!;
+                      });
+                    },
+                    items: repeatList
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value,
+                              style: TextStyle(color: Colors.grey)));
                     }).toList(),
                   ),
                 ),
