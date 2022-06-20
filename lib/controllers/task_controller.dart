643236f3d67ddbf2f3c58a6869bf5ac401de4ff4
pydalwi <yodialwiono13@gmail.com>
2022-06-20 08:44:30 +0700

    Add Button Delete&Close Task

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -7,10 +7,22 @@ import 'package:get/get.dart';
 class TaskController extends GetxController {
   @override
   void onReady() {
+    getTasks();
     super.onReady();
   }
 
+  var taskList = <Task>[].obs;
+
   Future<int> addTask({Task? task}) async {
     return await DBHelper.insert(task);
+  }
+
+  void getTasks() async {
+    List<Map<String, dynamic>> tasks = await DBHelper.query();
+    taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
+  }
+
+  void delete(Task task) {
+    DBHelper.delete(task);
   }
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,13 +1,17 @@
 import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_event1/controllers/task_controller.dart';
 import 'package:flutter_application_event1/services/notification_services.dart';
 import 'package:flutter_application_event1/ui/add_task_bar.dart';
 import 'package:flutter_application_event1/ui/theme.dart';
 import 'package:flutter_application_event1/ui/widgets/button.dart';
 import 'package:flutter_application_event1/services/theme_services.dart';
+import 'package:flutter_application_event1/ui/widgets/task_tile.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import '../models/task.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,6 +22,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DateTime _selectedDate = DateTime.now();
+  final _taskController = Get.put(TaskController());
   var notifyHelper;
   @override
   void initState() {
@@ -29,14 +34,138 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print("build method called");
     return Scaffold(
       appBar: AppBar(),
+      backgroundColor: context.theme.backgroundColor,
       body: Column(
         children: [
           _addTaskBar(),
           _addDateBar(),
+          SizedBox(height: 10),
+          _showTasks(),
         ],
       ),
+    );
+  }
+
+  _showTasks() {
+    return Expanded(
+      child: Obx(() {
+        return ListView.builder(
+            itemCount: _taskController.taskList.length,
+            itemBuilder: (_, index) {
+              print(_taskController.taskList.length);
+              return AnimationConfiguration.staggeredList(
+                  position: index,
+                  child: SlideAnimation(
+                      child: FadeInAnimation(
+                          child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            _showBottomSheet(
+                                context, _taskController.taskList[index]);
+                          },
+                          child: TaskTile(_taskController.taskList[index]))
+                    ],
+                  ))));
+            });
+      }),
+    );
+  }
+
+  _showBottomSheet(BuildContext context, Task task) {
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.only(top: 4),
+        height: task.isCompleted == 1
+            ? MediaQuery.of(context).size.height * 0.24
+            : MediaQuery.of(context).size.height * 0.32,
+        color: Get.isDarkMode ? darkGreyClr : Colors.white,
+        child: Column(
+          children: [
+            Container(
+              height: 6,
+              width: 120,
+              decoration: BoxDecoration(
+                color: Get.isDarkMode ? Colors.grey[600] : Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Spacer(),
+            task.isCompleted == 1
+                ? Container()
+                : _bottomSheetButton(
+                    label: "Task Completed",
+                    onTap: () {
+                      Get.back();
+                    },
+                    clr: primaryclr,
+                    context: context,
+                  ),
+            _bottomSheetButton(
+              label: "Delete Task",
+              onTap: () {
+                _taskController.delete(task);
+                _taskController.getTasks();
+                Get.back();
+              },
+              clr: Colors.red[300]!,
+              context: context,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            _bottomSheetButton(
+              label: "Close",
+              onTap: () {
+                Get.back();
+              },
+              clr: Colors.red[300]!,
+              isClose: true,
+              context: context,
+            ),
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _bottomSheetButton({
+    required String label,
+    required Function()? onTap,
+    required Color clr,
+    bool isClose = false,
+    required BuildContext context,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 4),
+          height: 55,
+          width: MediaQuery.of(context).size.width * 0.9,
+          decoration: BoxDecoration(
+              border: Border.all(
+                  width: 2,
+                  color: isClose == true
+                      ? Get.isDarkMode
+                          ? Colors.grey[600]!
+                          : Colors.grey[300]!
+                      : clr),
+              borderRadius: BorderRadius.circular(20),
+              color: isClose == true ? Colors.transparent : clr),
+          child: Center(
+            child: Text(
+              label,
+              style: isClose
+                  ? titleStyle
+                  : titleStyle.copyWith(color: Colors.white),
+            ),
+          )),
     );
   }
 
@@ -90,7 +219,12 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          MyButton(label: "+ Add Task", onTap: () => Get.to(AddTaskPage()))
+          MyButton(
+              label: "+ Add Task",
+              onTap: () async {
+                await Get.to(() => AddTaskPage());
+                _taskController.getTasks();
+              })
         ],
       ),
     );
